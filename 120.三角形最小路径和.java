8d97fb6de22d2c1fa113d6daa9545dac394d549f
chenxuan <mr.chenxuan@gmail.com>
2021-12-09 10:30:09 +0800

    2021.12.9

--- 120.三角形最小路径和.java
+++ 120.三角形最小路径和.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /*
  * @lc app=leetcode.cn id=120 lang=java
  *
@@ -6,8 +8,22 @@
 
 // @lc code=start
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
+    Integer[][] memo;
 
+    public int minimumTotal(List<List<Integer>> triangle) {
+        memo = new Integer[triangle.size()][triangle.size()];
+        return dfs(triangle, 0, 0);
+    }
+
+    public int dfs(List<List<Integer>> triangle, int i, int j) {
+        if (i == triangle.size()) return 0;
+
+        if (memo[i][j] == null) {
+            memo[i][j] = Math.min(dfs(triangle, i + 1, j), dfs(triangle, i + 1, j + 1)) 
+                + triangle.get(i).get(j);
+        }
+
+        return memo[i][j];
     }
 }
 // @lc code=end
