56d7d210a6013e42b18509dade1423b3e8b49349
chenxuan <mr.chenxuan@gmail.com>
2021-12-18 17:22:30 +0800

    2021.12.18

---
+++ 106.从中序与后序遍历序列构造二叉树.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=106 lang=java
+ *
+ * [106] 从中序与后序遍历序列构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
+// @lc code=end
+
---
+++ 655.输出二叉树.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=655 lang=java
+ *
+ * [655] 输出二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<String>> printTree(TreeNode root) {
+        int height = getHeight(root);
+        String[][] res = new String[height][(1 << height) - 1];
+        for (String[] arr : res) {
+            Arrays.fill(arr, "");
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+        fill(res, root, 0, 0, res[0].length);
+        for (String[] arr : res) {
+            ans.add(Arrays.asList(arr));
+        }
+        return ans;
+    }
+
+    public int getHeight(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(getHeight(node.right), getHeight(node.left)) + 1;
+    }
+
+    public void fill(String[][] res, TreeNode root, int i, int l, int r) {
+        if (root == null) return;
+        res[i][(l + r) / 2] = String.valueOf(root.val);
+        fill(res, root.left, i + 1, l, (l + r) / 2);
+        fill(res, root.right, i + 1, (l + r + 1) / 2,  r);
+    }
+}
+// @lc code=end
+
