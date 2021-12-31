c5add4de66d91ae51781f76add5543a996ea2277
chenxuan <mr.chenxuan@gmail.com>
2021-12-31 09:30:59 +0800

    2021.12.31

---
+++ 142.环形链表-ii.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> set = new HashSet<>();
+        while (pos != null) {
+            if (set.contains(pos)) {
+                return pos;
+            } else {
+                set.add(pos);
+            }
+            pos = pos.next;
+        }
+        return null;
+    }
+}
+// @lc code=end
+
