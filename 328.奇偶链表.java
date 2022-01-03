057b12b58e6f8d1eca60d8b8809b7862859e509a
chenxuan <mr.chenxuan@gmail.com>
2022-01-03 21:09:25 +0800

    2022.1.3

---
+++ 328.奇偶链表.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=java
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode odd = head, even = head.next, mark = head.next;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = mark;
+        return head;
+    }
+}
+// @lc code=end
+
