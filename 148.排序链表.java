736bc4b60d7a572a46dd70b21b87aa7052aeff37
chenxuan <mr.chenxuan@gmail.com>
2022-01-07 09:57:59 +0800

    2022.1.7

---
+++ 148.排序链表.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=java
+ *
+ * [148] 排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        // 利用快慢指针寻找中间节点，分割链表为2部分
+        ListNode mid = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            mid = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        mid.next = null;
+
+        // 第2段链表的头节点是slow
+        ListNode node1 = sortList(head);
+        ListNode node2 = sortList(slow);
+
+        // 合并排序后的2部分链表
+        return merge(node1, node2);
+    }
+
+    ListNode merge(ListNode node1, ListNode node2) {
+        ListNode dummy = new ListNode(0), temp = dummy;
+
+        while (node1 != null && node2 != null) {
+            if (node1.val < node2.val) {
+                temp.next = node1;
+                node1 = node1.next;
+            } else {
+                temp.next = node2;
+                node2 = node2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (node1 != null) temp.next = node1;
+        if (node2 != null) temp.next = node2;
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
---
+++ 160.相交链表.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+
+        // ab节点head互换，如果有相交节点则返回；如果没有，则同时到达null
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
+// @lc code=end
+
