eb0240f866342a83f65e308644dc3ea02a61d592
chenxuan <mr.chenxuan@gmail.com>
2022-01-09 22:38:52 +0800

    2022.1.9

---
+++ 445.两数相加-ii.java
@@ -0,0 +1,51 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode.cn id=445 lang=java
+ *
+ * [445] 两数相加 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Deque<Integer> stack1 = new LinkedList<>();
+        Deque<Integer> stack2 = new LinkedList<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode ans = null;
+
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int a = stack1.isEmpty() ? 0 : stack1.pop();
+            int b = stack2.isEmpty() ? 0 : stack2.pop();
+            int cur = a + b + carry;
+            carry = cur / 10;
+            cur = cur % 10;
+            ListNode curNode = new ListNode(cur);
+            curNode.next = ans;
+            ans = curNode;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
