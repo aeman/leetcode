64e873242766c7ebd1ad97cc51dac135dd9595b1
chenxuan <mr.chenxuan@gmail.com>
2022-01-15 20:21:50 +0800

    2022.1.15

---
+++ 147.对链表进行插入排序.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=147 lang=java
+ *
+ * [147] 对链表进行插入排序
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0, head), cur = head.next, sorted = head;
+
+        while (cur != null) {
+            if (sorted.val <= cur.val) {
+                sorted = sorted.next;
+            } else {
+                ListNode d = dummy;
+                while (d.next != null && d.next.val < cur.val) {
+                    d = d.next;
+                }
+                sorted.next = cur.next;
+                cur.next = d.next;
+                d.next = cur;
+            }
+            cur = sorted.next;
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
