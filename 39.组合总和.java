85bfd1bda96ef307153769c25532cd12d375ac78
chenxuan <mr.chenxuan@gmail.com>
2022-01-25 23:34:38 +0800

    2022.1.25

--- 39.组合总和.java
+++ 39.组合总和.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
  * @lc app=leetcode.cn id=39 lang=java
  *
@@ -7,7 +11,24 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
 
+    private void backtrack(List<List<Integer>> list, List<Integer> arrayList, int[] candidates, int target, int idx) {
+        if (target < 0) {
+            return;
+        } else if (target == 0) {
+            list.add(new ArrayList<>(arrayList));
+        } else {
+            for (int i = idx; i < candidates.length; i++) {
+                arrayList.add(candidates[i]);
+                backtrack(list, arrayList, candidates, target - candidates[i], i);
+                arrayList.remove(arrayList.size() - 1);
+            }
+        }
     }
 }
 // @lc code=end
