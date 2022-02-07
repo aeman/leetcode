0fd72dd36b43203402395614e1bf2bf99f37bac0
chenxuan <mr.chenxuan@gmail.com>
2022-02-07 13:52:24 +0800

    2022.2.7 list

---
+++ 2095.删除链表的中间节点.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=2095 lang=java
+ *
+ * [2095] 删除链表的中间节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode dummy = new ListNode(-1, head), slow = dummy, fast = dummy;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
+// @lc code=end
+
