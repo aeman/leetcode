fe50dfea39f643a981de0967b5513d3f9fa97733
chenxuan <mr.chenxuan@gmail.com>
2022-02-11 12:46:33 +0800

    2022.2.11 dp

---
+++ 53.最大子数组和.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+        }
+        // for (int i = 0; i < dp.length; i++) {
+        //     System.out.println(dp[i]);
+        // }
+
+        //先排序，然后找出最大值
+        Arrays.sort(dp);
+        return dp[n-1];
+    }
+}
+// @lc code=end
+
