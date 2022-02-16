4160ae2cba8ec63f21e93fdc8f96290c12018b9a
chenxuan <mr.chenxuan@gmail.com>
2022-02-16 11:59:49 +0800

    2022.2.16 dp

---
+++ 91.解码方法.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=91 lang=java
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        // '0' 的 dp[1] = 0
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
+// @lc code=end
