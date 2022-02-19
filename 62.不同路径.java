bfc6d85f5d846ca1b34921871901ad7cdb4c9e9f
chenxuan <mr.chenxuan@gmail.com>
2022-02-19 13:06:58 +0800

    2022.2.19 dp

--- 62.不同路径.java
+++ 62.不同路径.java
@@ -8,9 +8,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
+
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
+
         for (int i = 0; i < n; i++) {
             dp[0][i] = 1;
         }
