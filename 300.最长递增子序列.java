3270d474084ebd4dba0082d7d877fbed7b2e1546
chenxuan <mr.chenxuan@gmail.com>
2022-02-21 09:54:28 +0800

    2022.2.21 dp

---
+++ 300.最长递增子序列.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+        return dp[n - 1];
+    }
+}
+// @lc code=end
--- 5.最长回文子串.java
+++ 5.最长回文子串.java
@@ -1,3 +1,5 @@
+import java.security.acl.LastOwnerException;
+
 /*
  * @lc app=leetcode.cn id=5 lang=java
  *
@@ -7,8 +9,25 @@
 // @lc code=start
 class Solution {
     public String longestPalindrome(String s) {
+        int n = s.length(), begin = 0, maxLength = 0;
+        boolean[][] dp = new boolean[n][n];
 
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i <= j; i++) {
+                if (j - i < 2) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
+                }
+
+                if (dp[i][j] && maxLength < j - i + 1) {
+                    maxLength = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+
+        return s.substring(begin, begin + maxLength);
     }
 }
 // @lc code=end
-
