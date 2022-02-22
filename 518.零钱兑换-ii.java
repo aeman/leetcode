6dc48b61ed07ff59244d84c73807e2e7960428d3
chenxuan <mr.chenxuan@gmail.com>
2022-02-22 11:29:55 +0800

    2022.2.22 dp

---
+++ 518.零钱兑换-ii.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=java
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                for (int k = 0; k * coins[i - 1] <= j; k++) {
+                    dp[i][j] = dp[i][j] + dp[i - 1][j - coins[i - 1] * k];
+                }
+            }
+        }
+
+        return dp[n][amount];
+    }
+}
+// @lc code=end
