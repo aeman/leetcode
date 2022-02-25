d7129115013abe71541b657d8720d12026ef17b7
chenxuan <mr.chenxuan@gmail.com>
2022-02-25 09:07:11 +0800

    2022.2.25 array

---
+++ 153.寻找旋转排序数组中的最小值.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=153 lang=java
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+// @lc code=start
+class Solution {
+    public int findMin(int[] nums) {
+        Arrays.sort(nums);
+        return nums[0];
+    }
+}
+// @lc code=end
+
---
+++ 238.除自身以外数组的乘积.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=238 lang=java
+ *
+ * [238] 除自身以外数组的乘积
+ */
+
+// @lc code=start
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int[] left = new int[n], right = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = nums[i - 1] * left[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] * right[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+}
+// @lc code=end
