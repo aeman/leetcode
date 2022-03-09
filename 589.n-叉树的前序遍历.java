8f3dbdd38bca04e478c3b25fcf301da806904e9c
chenxuan <mr.chenxuan@gmail.com>
2022-03-10 07:43:14 +0800

    2022.3.10 daily

---
+++ 589.n-叉树的前序遍历.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=589 lang=java
+ *
+ * [589] N 叉树的前序遍历
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+
+    private void preorder(Node root, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        for (Node node : root.children) {
+            preorder(node, list);
+        }
+    }
+}
+// @lc code=end
+
