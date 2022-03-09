7ddfa215017962257385462afc56f9dd63a10c83
chenxuan <mr.chenxuan@gmail.com>
2022-03-09 16:13:39 +0800

    2022.3.9 prefix sum

--- 560.和为-k-的子数组.java
+++ 560.和为-k-的子数组.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 /*
  * @lc app=leetcode.cn id=560 lang=java
@@ -9,28 +10,31 @@ import java.util.HashMap;
 // @lc code=start
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sum = 0;
-        int count = 0;
+        int n = nums.length, ans = 0, pre = 0;
 
-        // for (int i = 0; i < nums.length; i++) {
-        // sum = 0;
-        // for (int j = i; j < nums.length; j++) {
-        // sum += nums[j];
-        // if (sum == k) {
-        // count++;
+        // int[] preSum = new int[n + 1];
+        // for (int i = 1; i <= n; i++) {
+        //     preSum[i] = preSum[i - 1] + nums[i - 1];
         // }
+
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 0; j < i; j++) {
+        //         if (preSum[i] - preSum[j] == k)
+        //             ans++;
+        //     }
         // }
-        // }
-        HashMap<Integer, Integer> map = new HashMap<>();
+
+        Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
 
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k))
-                count += map.get(sum - k);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        for (int i = 0; i < n; i++) {
+            pre += nums[i];
+            if (map.containsKey(pre - k))
+                ans += map.get(pre - k);
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
         }
-        return count;
+        
+        return ans;
     }
 }
 // @lc code=end
