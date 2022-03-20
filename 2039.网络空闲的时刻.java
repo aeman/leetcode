b3f19999abf159fe2677294bd1887e0baea297e3
chenxuan <mr.chenxuan@gmail.com>
2022-03-20 12:54:49 +0800

    2022.3.20 daily

---
+++ 2039.网络空闲的时刻.java
@@ -0,0 +1,50 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode.cn id=2039 lang=java
+ *
+ * [2039] 网络空闲的时刻
+ */
+
+// @lc code=start
+class Solution {
+    public int networkBecomesIdle(int[][] edges, int[] patience) {
+        int n = patience.length;
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; ++i) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        boolean[] visit = new boolean[n];
+        for (int[] v : edges) {
+            adj[v[0]].add(v[1]);
+            adj[v[1]].add(v[0]);
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(0);
+        visit[0] = true;
+        int dist = 1;
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int curr = queue.poll();
+                for (int v : adj[curr]) {
+                    if (visit[v]) {
+                        continue;
+                    }
+                    queue.offer(v);
+                    int time = patience[v] * ((2 * dist - 1) / patience[v]) + 2 * dist + 1;
+                    ans = Math.max(ans, time);
+                    visit[v] = true;
+                }
+            }
+            dist++;
+        }
+        return ans;
+    }
+}
+// @lc code=end
