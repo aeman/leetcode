ec92a6ed4e372c311fe9325db5720e762c28d6de
chenxuan <mr.chenxuan@gmail.com>
2022-03-22 16:13:14 +0800

    2022.3.22 backtrack

--- 46.全排列.java
+++ 46.全排列.java
@@ -18,14 +18,15 @@ class Solution {
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
         if (tempList.size() == nums.length) {
             list.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (tempList.contains(nums[i])) continue;
+            return;
+        }
 
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
-            }
+        for (int i = 0; i < nums.length; i++) {
+            if (tempList.contains(nums[i])) continue;
+
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums);
+            tempList.remove(tempList.size() - 1);
         }
     }
 }
