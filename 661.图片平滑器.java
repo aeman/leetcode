01cffd1a4c1312f5a15e8dfafe79792468506acb
chenxuan <mr.chenxuan@gmail.com>
2022-03-24 09:18:06 +0800

    2022.3.24 daily

---
+++ 661.图片平滑器.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=661 lang=java
+ *
+ * [661] 图片平滑器
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] newImg = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                newImg[i][j] = smoothe(img, i, j, m, n);
+            }
+        }
+
+        return newImg;
+    }
+
+    private int smoothe(int[][] img, int x, int y, int m, int n) {
+        int num = 0, sum = 0;
+
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <=  y + 1; j++) {
+                if (i >= 0 && j >= 0 && i < m && j < n) {
+                    num++;
+                    sum += img[i][j];
+                }
+            }
+        }
+
+        return sum / num;
+    }
+}
+// @lc code=end
+
