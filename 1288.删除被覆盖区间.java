8df0ce1ebc5b61b27d11b9e7a66f23d82cd3f708
chenxuan <mr.chenxuan@gmail.com>
2022-03-31 18:19:52 +0800

    2022.3.31 interval

--- 1288.删除被覆盖区间.java
+++ 1288.删除被覆盖区间.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=1288 lang=java
  *
@@ -7,7 +9,19 @@
 // @lc code=start
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
+        int curEnd, prevEnd = 0, count = 0;
+        // 先按开始位置排序；如果开始位置相同，则结束位置大的排前面
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
 
+        for (int[] interval : intervals) {
+            curEnd = interval[1];
+            if (curEnd > prevEnd) {
+                count++;
+                prevEnd = curEnd;
+            } 
+        }
+
+        return count;
     }
 }
 // @lc code=end
