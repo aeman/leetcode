b00b99e40e3c477723a04cc8892d4b459cdf8cdf
chenxuan <mr.chenxuan@gmail.com>
2022-04-01 14:53:13 +0800

    2022.4.1 2sum

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -7,17 +10,16 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
+            map.put(nums[i], i);
         }
-        return result;
+
+        return new int[0];
     }
 }
 // @lc code=end
