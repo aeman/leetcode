a08dbcf13bf5efe45fa78906cfdbadca6e0ca988
chenxuan <mr.chenxuan@gmail.com>
2022-04-06 12:41:08 +0800

    2022.4.6 two points

--- 76.最小覆盖子串.java
+++ 76.最小覆盖子串.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=76 lang=java
  *
@@ -6,8 +10,52 @@
 
 // @lc code=start
 class Solution {
+    Map<Character, Integer> ori = new HashMap<>();
+    Map<Character, Integer> cnt = new HashMap<>();
+    
     public String minWindow(String s, String t) {
+        int sLen = s.length(), tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int left = 0, right = -1;
 
+        while (right < sLen) {
+            right++;
+            if (right < sLen && ori.containsKey(s.charAt(right))) {
+                cnt.put(s.charAt(right), cnt.getOrDefault(s.charAt(right), 0) + 1);
+            }
+
+            while (check() && left <= right) {
+                if (right - left + 1 < len) {
+                    len = right - left + 1;
+                    ansL = left;
+                    ansR = left + len;
+                }
+                if (ori.containsKey(s.charAt(left))) {
+                    cnt.put(s.charAt(left), cnt.getOrDefault(s.charAt(left), 0) - 1);
+                }
+                left++;
+            }
+        }
+
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+        Iterator iter = ori.entrySet().iterator(); 
+        while (iter.hasNext()) { 
+            Map.Entry entry = (Map.Entry) iter.next(); 
+            Character key = (Character) entry.getKey(); 
+            Integer val = (Integer) entry.getValue(); 
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        } 
+        return true;
     }
 }
 // @lc code=end
