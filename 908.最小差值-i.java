c0cbd54e71116bf97e59829d4d4341d87615b204
chenxuan <mr.chenxuan@gmail.com>
2022-04-30 09:13:22 +0800

    2022.4.30 daily

---
+++ 908.最小差值-i.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=908 lang=java
+ *
+ * [908] 最小差值 I
+ */
+
+// @lc code=start
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        int diff = (max - k) - (min + k);
+        return diff < 0 ? 0 : diff;
+    }
+}
+// @lc code=end
+
