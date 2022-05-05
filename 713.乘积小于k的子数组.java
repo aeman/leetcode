134959bd4f1c07e8ec584191ab15998dd35f2f97
chenxuan <mr.chenxuan@gmail.com>
2022-05-05 11:01:57 +0800

    2022.5.5 daily

---
+++ 713.乘积小于k的子数组.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=713 lang=java
+ *
+ * [713] 乘积小于K的子数组
+ */
+
+// @lc code=start
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int ans = 0, left = 0, prod = 1;
+
+        for (int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while (prod >= k) {
+                prod /= nums[left++];
+            }
+            ans += right - left + 1;
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
