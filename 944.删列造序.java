4f4c14b9fe037ce89cd9cdc7e0d7c1b9c2bf3ff1
chenxuan <mr.chenxuan@gmail.com>
2022-05-12 14:22:00 +0800

    2022.5.12 daily

---
+++ 944.删列造序.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=944 lang=java
+ *
+ * [944] 删列造序
+ */
+
+// @lc code=start
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int m = strs.length, n = strs[0].length(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
