6854d62e7c3802675776d9c39afc91459de4c38d
chenxuan <mr.chenxuan@gmail.com>
2022-05-30 16:08:30 +0800

    2022.5.30 graph union

--- 547.省份数量.java
+++ 547.省份数量.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /*
  * @lc app=leetcode.cn id=547 lang=java
  *
@@ -7,18 +10,63 @@
 // @lc code=start
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean[] visited = new boolean[n];
-        int circle = 0;
+        int n = isConnected.length, circle = n;
+        // boolean[] visited = new boolean[n];
+        
+        int[] fathers = new int[n];
+        for (int i = 0; i < fathers.length; i++) {
+            fathers[i] = i;
+        }
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(isConnected, visited, n, i);
-                circle++;
+            for (int j = i + 1; j < fathers.length; j++) {
+                if (isConnected[i][j] == 1 && union(fathers, i, j)) {
+                    circle--;
+                }
             }
         }
 
+        // for (int i = 0; i < n; i++) {
+        //     if (!visited[i]) {
+        //         bfs(isConnected, visited, n, i);
+        //         dfs(isConnected, visited, n, i);
+        //         circle++;
+        //     }
+        // }
+
         return circle;
+    }
+
+    private boolean union(int[] fathers, int i, int j) {
+        int fatherI = findFather(fathers, i);
+        int fatherJ = findFather(fathers, j);
+        if (fatherI != fatherJ) {
+            fathers[fatherI] = fatherJ;
+            return true;
+        }
+        return false;
+    }
+
+    private int findFather(int[] fathers, int i) {
+        if (fathers[i] != i) {
+            fathers[i] = findFather(fathers, fathers[i]);
+        }
+        return fathers[i];
+    }
+
+    private void bfs(int[][] isConnected, boolean[] visited, int n, int i) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(i);
+        visited[i] = true;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int j = 0; j < n; j++) {
+                if (isConnected[cur][j] == 1 && !visited[j]) {
+                    queue.offer(j);
+                    visited[j] = true;
+                }
+            }
+        }
     }
 
     private void dfs(int[][] isConnected, boolean[] visited, int n, int i) {
--- 839.相似字符串组.java
+++ 839.相似字符串组.java
@@ -7,8 +7,47 @@
 // @lc code=start
 class Solution {
     public int numSimilarGroups(String[] strs) {
+        int n = strs.length, groups = n;
+        int[] fathers = new int[n];
+        for (int i = 0; i < n; i++) {
+            fathers[i] = i;
+        }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (similar(strs[i], strs[j]) && union(fathers, i, j)) {
+                    groups--;
+                }
+            }
+        }
+
+        return groups;
+    }
+
+    private boolean similar(String s1, String s2) {
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i))
+                diff++;
+        }
+        return diff <= 2;
+    }
+
+    private boolean union(int[] fathers, int i, int j) {
+        int fatherI = findFather(fathers, i);
+        int fatherJ = findFather(fathers, j);
+        if (fatherI != fatherJ) {
+            fathers[fatherI] = fatherJ;
+            return true;
+        }
+        return false;
+    }
+
+    private int findFather(int[] fathers, int i) {
+        if (fathers[i] != i) {
+            fathers[i] = findFather(fathers, fathers[i]);
+        }
+        return fathers[i];
     }
 }
 // @lc code=end
-
