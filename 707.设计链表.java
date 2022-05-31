60874e59ad194b756d8c0fcbe17e28de6c563d67
chenxuan <mr.chenxuan@gmail.com>
2022-05-31 13:57:54 +0800

    2022.5.31 linked list

--- 707.设计链表.java
+++ 707.设计链表.java
@@ -6,29 +6,63 @@
 
 // @lc code=start
 class MyLinkedList {
+    ListNode head;
+    int size;
 
     public MyLinkedList() {
-
+        this.head = new ListNode(0);
+        this.size = 0;
     }
     
     public int get(int index) {
+        if (index < 0 || index >= size) return -1;
 
+        ListNode cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
     }
     
     public void addAtHead(int val) {
-
+        addAtIndex(0, val);;
     }
     
     public void addAtTail(int val) {
-
+        addAtIndex(size, val);
     }
     
     public void addAtIndex(int index, int val) {
+        if (index > size) return;
 
+        ListNode node = new ListNode(val);
+        ListNode cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        node.next = cur.next;
+        cur.next = node;
+        size++;
     }
     
     public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        
+        ListNode cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        size--;
+    }
 
+    class ListNode {
+        int val;
+        ListNode next;
+        
+        public ListNode(int val) {
+            this.val = val;
+        }
     }
 }
 
