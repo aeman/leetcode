28f08d9cf329155e4a394b97b30cfc47bb59b43b
chenxuan <mr.chenxuan@gmail.com>
2022-06-09 09:31:25 +0800

    2022.6.9 daily

--- 1049.最后一块石头的重量-ii.java
+++ 1049.最后一块石头的重量-ii.java
@@ -9,22 +9,22 @@ class Solution {
     public int lastStoneWeightII(int[] stones) {
         int n = stones.length;
         int sum = 0;
-        for (int stone : stones)
-            sum += stone;
+        for (int stone : stones) sum += stone;
 
+        // 尽量让石头分成重量相同的两堆，相撞之后剩下的石头最小
         int target = sum / 2;
         int[][] dp = new int[n + 1][target + 1];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {  // 遍历石头
             int stone = stones[i - 1];
-            for (int j = 0; j <= target; j++) {
+            for (int j = 0; j <= target; j++) { // 遍历背包
                 dp[i][j] = dp[i - 1][j];
                 if (j >= stone)
                     dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - stone] + stone);
             }
         }
 
-        return Math.abs(sum - dp[n][target] - dp[n][target]);
+        return Math.abs((sum - dp[n][target]) - dp[n][target]);
     }
 }
 // @lc code=end
--- 416.分割等和子集.java
+++ 416.分割等和子集.java
@@ -15,24 +15,26 @@ class Solution {
         //如果 maxNum>target，则除了 maxNum 以外的所有元素之和一定小于 target，因此不可能将数组分割成元素和相等的两个子集
         if (max > target) return false;
 
-        boolean[][] dp = new boolean[n][target + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
+        int[][] dp = new int[n][target + 1];
+        for (int i = 0; i <= target; i++) {
+            dp[0][i] = i >= nums[0] ? nums[0] : 0;
         }
 
-        dp[0][nums[0]] = true;
         for (int i = 1; i < n; i++) {
             int num = nums[i];
-            for (int j = 1; j <= target; j++) {
-                if (j >= num) {
-                    dp[i][j] =  dp[i - 1][j] || dp[i - 1][j - num];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+            for (int j = 0; j <= target; j++) {
+                int no = dp[i - 1][j];
+                int yes = j >= num ? dp[i - 1][j - num] + num :  0;
+                dp[i][j] = Math.max(no, yes);
+                // if (j >= num) {
+                //     dp[i][j] =  dp[i - 1][j] || dp[i - 1][j - num];
+                // } else {
+                //     dp[i][j] = dp[i - 1][j];
+                // }
             }
         }
 
-        return dp[n - 1][target];
+        return dp[n - 1][target] == target;
     }
 }
 // @lc code=end
