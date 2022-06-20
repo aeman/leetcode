ae59b1a7d28f7242a4e3cad88043348b37bb7fca
chenxuan <mr.chenxuan@gmail.com>
2022-06-20 11:21:40 +0800

    2022.6.20 graph union find

--- 684.冗余连接.java
+++ 684.冗余连接.java
@@ -11,22 +11,24 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         parent = new int[edges.length + 1];
 
+        // 初始化各节点的顶点为自己
         for (int i = 0; i < parent.length; i++) {
             parent[i] = i;
         }
 
         for (int[] edge : edges) {
+            // 如果两个点的顶点已经连通，说明加上这条边会导致环出现，返回
             if (find(edge[0]) == find(edge[1]))
                 return edge;
             else
-                union(edge[0], edge[1]);
+                union(edge[0], edge[1]);    // 连通这两个节点为同一顶点
         }
 
         return edges[0];
     }
 
     private void union(int x, int y) {
-        parent[find(y)] = find(x);
+        parent[find(y)] = find(x);      // 把x连接到y上
     }
 
     private int find(int x) {
--- 685.冗余连接-ii.java
+++ 685.冗余连接-ii.java
@@ -7,7 +7,64 @@
 // @lc code=start
 class Solution {
     public int[] findRedundantDirectedConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n + 1);
 
+        int[] parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int conflict = -1, cycle = -1;
+        for (int i = 0; i < n; i++) {
+            int x = edges[i][0], y = edges[i][1];
+            if (parent[y] != y) {
+                conflict = i;
+            } else {
+                parent[y] = x;
+                if (uf.find(x) == uf.find(y)) {
+                    cycle = i;
+                } else {
+                    uf.union(x, y);
+                }
+            }
+        }
+
+        if (conflict < 0) {
+            int[] redundant = {edges[cycle][0], edges[cycle][1]};
+            return redundant;
+        } else {
+            int[] conflictEdge = edges[conflict];
+            if (cycle >= 0) {
+                int[] redundant = {parent[conflictEdge[1]], conflictEdge[1]};
+                return redundant;
+            } else {
+                int[] redundant = {conflictEdge[0], conflictEdge[1]};
+                return redundant;
+            }
+        }
+    }
+
+    class UnionFind {
+        private int[] parent;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            parent[find(x)] = find(y);
+        }
     }
 }
 // @lc code=end
