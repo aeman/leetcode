68d723e7f7576ca455ce06f06db2ce1879806bd0
chenxuan <mr.chenxuan@gmail.com>
2022-06-21 08:14:51 +0800

    2022.6.21 daily happy day

--- 1108.ip-地址无效化.java
+++ 1108.ip-地址无效化.java
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replaceAll("\\.", "[.]");
+        return address.replace(".", "[.]");
     }
 }
 // @lc code=end
