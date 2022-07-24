b81fe8a91280d23a2c48853f5460f7b8df1d766c
chenxuan <mr.chenxuan@gmail.com>
2022-07-24 19:03:01 +0800

    2022.7.24 daily

---
+++ 1184.公交站间的距离.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1184 lang=java
+ *
+ * [1184] 公交站间的距离
+ */
+
+// @lc code=start
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        if (start > destination) {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < distance.length; i++) {
+            if (i >= start && i < destination) {
+                sum1 += distance[i];
+            } else {
+                sum2 += distance[i];
+            }
+        }
+
+        return Math.min(sum1, sum2);
+    }
+}
+// @lc code=end
+
