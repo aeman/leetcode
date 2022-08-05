ffdbc64fc814c02a54818749468f2b7169a83982
chenxuan <mr.chenxuan@gmail.com>
2022-08-05 23:45:32 +0800

    2022.8.5 daily

---
+++ 623.在二叉树中增加一行.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=623 lang=java
+ *
+ * [623] 在二叉树中增加一行
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) return null;
+        if (depth == 1) return new TreeNode(val, root, null);
+
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        } else {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+
+        return root;
+    }
+}
+// @lc code=end
+
