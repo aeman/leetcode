4fcb82fe2fdb669b0150e0648af2f83caf4077d2
chenxuan <mr.chenxuan@gmail.com>
2022-08-08 11:38:52 +0800

    2022.8.8 daily

---
+++ 761.特殊的二进制序列.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=761 lang=java
+ *
+ * [761] 特殊的二进制序列
+ */
+
+// @lc code=start
+class Solution {
+    public String makeLargestSpecial(String s) {
+        if (s.length() <= 2) return s;
+
+        int cnt = 0, left = 0;
+        List<String> subs = new ArrayList<String>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                ++cnt;
+            } else {
+                --cnt;
+                if (cnt == 0) {
+                    subs.add("1" + makeLargestSpecial(s.substring(left + 1, i)) + "0");
+                    left = i + 1;
+                }
+            }
+        }
+
+        Collections.sort(subs, (a, b) -> b.compareTo(a));
+        StringBuilder ans = new StringBuilder();
+        for (String sub : subs) {
+            ans.append(sub);
+        }
+        return ans.toString();
+    }
+}
+// @lc code=end
