7f977b69103ddb43444ed09570898452e6bbd52c
chenxuan <mr.chenxuan@gmail.com>
2022-10-22 15:56:44 +0800

    2022.10.22 daily

---
+++ 1235.规划兼职工作.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=1235 lang=java
+ *
+ * [1235] 规划兼职工作
+ */
+
+// @lc code=start
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[] { startTime[i], endTime[i], profit[i] };
+        }
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int k = binarySearch(jobs, i - 1, jobs[i - 1][0]);
+            dp[i] = Math.max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+        }
+        return dp[n];
+    }
+
+    private int binarySearch(int[][] jobs, int right, int target) {
+        int left = 0;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (jobs[mid][1] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+// @lc code=end
