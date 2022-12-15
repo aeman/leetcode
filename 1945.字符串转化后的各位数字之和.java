8202109fb71a4bd65f2bebc93700b83f539fcc5a
chenxuan <mr.chenxuan@gmail.com>
2022-12-15 22:22:15 +0800

    2022.12.15 daily

---
+++ 1945.字符串转化后的各位数字之和.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1945 lang=java
+ *
+ * [1945] 字符串转化后的各位数字之和
+ */
+
+// @lc code=start
+class Solution {
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            sb.append(ch - 'a' + 1);
+        }
+        String digits = sb.toString();
+        for (int i = 1; i <= k && digits.length() > 1; ++i) {
+            int sum = 0;
+            for (int j = 0; j < digits.length(); ++j) {
+                char ch = digits.charAt(j);
+                sum += ch - '0';
+            }
+            digits = Integer.toString(sum);
+        }
+
+        return Integer.parseInt(digits);
+    }
+}
+// @lc code=end
