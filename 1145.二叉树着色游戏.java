f34e1e6c620b66007efa0afbad65bc04de711992
chenxuan <mr.chenxuan@gmail.com>
2023-02-03 17:43:52 +0800

    2023.2.3 daily

---
+++ 1145.二叉树着色游戏.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=1145 lang=java
+ *
+ * [1145] 二叉树着色游戏
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode xNode;
+
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        find(root, x);
+        int leftSize = getSubtreeSize(xNode.left);
+        if (leftSize >= (n + 1) / 2) {
+            return true;
+        }
+        int rightSize = getSubtreeSize(xNode.right);
+        if (rightSize >= (n + 1) / 2) {
+            return true;
+        }
+        int remain = n - 1 - leftSize - rightSize;
+        return remain >= (n + 1) / 2;
+    }
+
+    private int getSubtreeSize(TreeNode node) {
+        if (node == null) return 0;
+        return 1 + getSubtreeSize(node.left) + getSubtreeSize(node.right);
+    }
+
+    private void find(TreeNode node, int x) {
+        if (xNode != null || node == null) {
+            return;
+        }
+        if (node.val == x) {
+            xNode = node;
+            return;
+        }
+        find(node.left, x);
+        find(node.right, x);
+    }
+}
+// @lc code=end
+
