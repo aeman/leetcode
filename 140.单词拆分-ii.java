660ffdff5e882cbc25c6b77ca5f6a63e80881b82
chenxuan <mr.chenxuan@gmail.com>
2023-02-08 16:25:31 +0800

    2023.2.8 chatgpt

--- 140.单词拆分-ii.java
+++ 140.单词拆分-ii.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /*
  * @lc app=leetcode.cn id=140 lang=java
  *
@@ -7,7 +12,46 @@
 // @lc code=start
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        
+        Set<String> wordSet = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        List<String> res = new ArrayList<>();
+        if (!dp[n]) {
+            return res;
+        }
+        dfs(s, wordSet, n, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void dfs(String s, Set<String> wordSet, int end, List<String> cur, List<String> res) {
+        if (end == 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = cur.size() - 1; i >= 0; i--) {
+                sb.append(cur.get(i));
+                if (i > 0) {
+                    sb.append(" ");
+                }
+            }
+            res.add(sb.toString());
+            return;
+        }
+        for (int i = 0; i < end; i++) {
+            String sub = s.substring(i, end);
+            if (wordSet.contains(sub)) {
+                cur.add(sub);
+                dfs(s, wordSet, i, cur, res);
+                cur.remove(cur.size() - 1);
+            }
+        }
     }
 }
 // @lc code=end
---
+++ 25.k-个一组翻转链表.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=java
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (head != null) {
+            ListNode tail = pre;
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
+                    return dummy.next;
+                }
+            }
+            ListNode next = tail.next;
+            ListNode[] reverseNodes = reverse(head, tail);
+            head = reverseNodes[0];
+            tail = reverseNodes[1];
+            pre.next = head;
+            tail.next = next;
+            pre = tail;
+            head = tail.next;
+        }
+        return dummy.next;
+    }
+    private ListNode[] reverse(ListNode head, ListNode tail) {
+        ListNode pre = tail.next;
+        ListNode cur = head;
+        while (pre != tail) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return new ListNode[] {tail, head};
+    }
+}
+// @lc code=end
+
